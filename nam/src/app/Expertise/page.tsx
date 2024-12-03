6f4aa1c813ed488bff364317e40da920769c12a1
genie360s <alexgmkwizu@gmail.com>
2024-12-03 14:26:45 +0300

    create: footer component

---
+++ nam/src/app/Expertise/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Image from "next/image";
+import SearchBar from "../components/searchBar";
+import { NavBar } from "../components/navBar";
+import { Footer } from "../components/footer";
+
+export default function Expertise() {
+  return (
+    <div>
+      <div className="flex justify-between mx-auto mt-20 size-16 w-[80vw]">
+        <div className="h-96 flex-none">
+          <Image
+            src="/images/nam-logo.png"
+            alt="Nam & Associates"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className=" flex-auto order-last">
+          <div className="grid justify-items-end mt-3">
+            <SearchBar
+              searchHandler={(query: string) => console.log(query)}
+            ></SearchBar>
+          </div>
+        </div>
+      </div>
+      <NavBar />
+      {/* footer */}
+      <Footer />
+    </div>
+  );
+}
--- nam/src/app/page.tsx
+++ nam/src/app/page.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import SearchBar from "./components/searchBar";
 import { NavBar } from "./components/navBar";
 import GoogleMapEmbed from "./components/googleMapEmbed";
+import { Footer } from "./components/footer";
 import { Borel } from "next/font/google";
 
 const borel = Borel({
   subsets: ["latin"],
   weight: ["400"], // Specify desired font weights
 });
-const currentYear = new Date().getFullYear(); // Get the current year dynamically
+
 
 export default function Home() {
   return (
@@ -355,47 +356,7 @@ export default function Home() {
       <div className="mx-auto w-[80vw] h-[50vh] mt-[5vh]">
         <GoogleMapEmbed />
       </div>
-
-      <footer className="grid grid-cols-4 w-[80vw] mx-auto gap-4 mt-[10ch] py-[6vh] border-t-2 border-b-2 border-brand-copper ">
-        <div className=" cols-1">
-        <Image
-            src="/images/nam-logo.png"
-            alt="Nam & Associates"
-            width={200}
-            height={200}
-          />
-        </div>
-        <div className="cols-1">
-          <div className="grid grid-rows">
-            <div className="row font-medium text-lg text-off-white">Useful Links</div>
-            <div className="row text-off-white mt-4"><i className="bi bi-box-arrow-up-right"></i> Case Studies</div>
-            <div className="row text-off-white mt-2"><i className="bi bi-box-arrow-up-right"></i> Areas Of Practice</div>
-            <div className="row text-off-white mt-2"> <i className="bi bi-box-arrow-up-right"></i> Blogs</div>
-          </div>
-        </div>
-        <div className="cols-1">
-          <div className="grid grid-rows">
-            <div className="row font-medium text-lg text-off-white">Contact Us</div>
-            <div className="row text-off-white mt-4"><i className="bi bi-envelope"></i> info@namassociates.co.tz</div>
-            <div className="row text-off-white mt-2"><i className="bi bi-telephone-inbound"></i> +255(0)753678678</div>
-          </div>
-        </div>
-        <div className="cols-1">
-          <div className="grid grid-rows">
-            <div className="row font-medium text-lg text-off-white">Follow Us</div>
-            <div className="grid row grid-cols-3 gap-2 font-medium text-lg text-off-white">
-            <div className="cols-1 text-off-white mt-4"><i className="bi bi-twitter"></i> </div>
-            <div className="cols-1 text-off-white mt-4"><i className="bi bi-instagram"></i> </div>
-            <div className="cols-1 text-off-white mt-4"><i className="bi bi-linkedin"></i> </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-      <div className="mx-auto w-[80vw]">
-        <div className="text-center text-off-white text-sm mt-4">
-        Copyright © {currentYear} NAM & Associates
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
