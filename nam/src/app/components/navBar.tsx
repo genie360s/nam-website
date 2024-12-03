5415720c2912196bd9db52524c7364497aa474c6
genie360s <alexgmkwizu@gmail.com>
2024-12-03 16:35:12 +0300

    fixed:navbar links

--- nam/src/app/components/navBar.tsx
+++ nam/src/app/components/navBar.tsx
@@ -1,20 +1,28 @@
+//components/NavBar.tsx
+import Link from "next/link";
 export function NavBar() {
   return (
-    
-      <div className="mx-auto flex justify-center gap-x-4 w-[80vw] py-2 ">
+    <div className="mx-auto flex justify-center gap-x-4 w-[80vw] py-2 ">
+      <Link href="/">
         <div className="bg-transparent border border-brand-copper rounded-full py-1 text-center  text-brand-copper w-48">
           Home
         </div>
+      </Link>
+      <Link href="/Expertise">
         <div className="bg-transparent border border-brand-copper rounded-full py-1 text-center  text-off-white hover:text-brand-copper w-48">
           Our Expertise
         </div>
+      </Link>
+      <Link href="/Legal">
         <div className="bg-transparent border border-brand-copper rounded-full py-1 text-center text-off-white hover:text-brand-copper  w-48">
           Legal Practice
         </div>
+      </Link>
+      <Link href="/Legal">
         <div className="bg-transparent border border-brand-copper rounded-full py-1 text-center text-off-white hover:text-brand-copper  w-48">
           Blogs
         </div>
-      </div>
- 
+      </Link>
+    </div>
   );
 }
